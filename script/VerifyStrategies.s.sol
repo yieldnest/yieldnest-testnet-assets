227318b7b36eaec3015a453cd22346022f91045a
danoctavian <danoctavian91@gmail.com>
2024-08-29 00:27:45 +0300

    add strategies

--- script/VerifyStrategies.s.sol
+++ script/VerifyStrategies.s.sol
@@ -3,24 +3,31 @@ pragma solidity ^0.8.20;
 
 import "forge-std/Script.sol";
 import "forge-std/console.sol";
-import "../src/interfaces/IStrategy.sol";
 import "../src/YnAsset.sol";
+
+interface IStrategy {
+    /// @notice The underlying token for shares in this Strategy
+    function underlyingToken() external view returns (address);
+}
+
 
 contract VerifyStrategies is Script {
     function run() external {
-        address strategyAddress = 0x6C50dfEdD1AFfEB9610b91e4A73a4424D6CB3494;
-        address ynBTCAddress = 0x810615698eeAEE37efA98F821411aACe4e0d14e5;
+        verifyStrategy(0x6C50dfEdD1AFfEB9610b91e4A73a4424D6CB3494, 0x810615698eeAEE37efA98F821411aACe4e0d14e5, "ynBTC");
+        verifyStrategy(0x1b4F6959f073F5eD89b1169cC365A2279B3e45DE, 0xf1BD6f0da70926d0d4c9Ed76ef4DBF6963972a13, "ynSBTC");
+    }
 
+    function verifyStrategy(address strategyAddress, address tokenAddress, string memory tokenName) internal {
         IStrategy strategy = IStrategy(strategyAddress);
-        YnAsset ynBTC = YnAsset(ynBTCAddress);
+        YnAsset token = YnAsset(tokenAddress);
 
         address underlyingToken = strategy.underlyingToken();
 
-        if (underlyingToken == address(ynBTC)) {
-            console.log("Verification successful: Strategy is for ynBTC");
+        if (underlyingToken == address(token)) {
+            console.log("Verification successful: Strategy is for ", tokenName);
         } else {
-            console.log("Verification failed: Strategy is not for ynBTC");
-            console.log("Expected: ", address(ynBTC));
+            console.log("Verification failed: Strategy is not for ", tokenName);
+            console.log("Expected: ", address(token));
             console.log("Actual: ", underlyingToken);
         }
     }
