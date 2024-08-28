465a86c66cda527a328ed73d9c5cf9c3892a7289
danoctavian <danoctavian91@gmail.com>
2024-08-28 21:36:59 +0300

    add role

--- src/YnAsset.sol
+++ src/YnAsset.sol
@@ -5,23 +5,31 @@ import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/token/ERC20/extensions/ERC20BurnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/access/AccessControlUpgradeable.sol";
 
-contract YnAsset is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable {
+
+contract YnAsset is Initializable, ERC20Upgradeable, ERC20BurnableUpgradeable, OwnableUpgradeable, AccessControlUpgradeable {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+
     function initialize(string memory name, string memory symbol, address owner) initializer public {
         __ERC20_init(name, symbol);
         __ERC20Burnable_init();
         __Ownable_init(owner);
+        __AccessControl_init();
+
+        _grantRole(DEFAULT_ADMIN_ROLE, owner);
+        _grantRole(MINTER_ROLE, owner);
     }
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
     }
 
-    function burn(uint256 amount) public override onlyOwner {
+    function burn(uint256 amount) public override onlyRole(MINTER_ROLE) {
         super.burn(amount);
     }
 
-    function burnFrom(address account, uint256 amount) public override onlyOwner {
+    function burnFrom(address account, uint256 amount) public override onlyRole(MINTER_ROLE) {
         super.burnFrom(account, amount);
     }
 }
